e0b8aafa505bdbc9785248d9979b53cc9d558fb2
e-Smajic <esmajic2@etf.unsa.ba>
2021-10-16 20:50:33 +0200

    Prvi program sa predavanja

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,31 @@
 package ba.unsa.etf.rpr;
+import java.util.Scanner;
 
 public class Main {
+    static int sumaCifara (int num) {
+        int suma = 0;
+        while (num != 0) {
+            suma += num % 10;
+            num /= 10;
+        }
+        return suma;
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Unesite broj: ");
+        int number = input.nextInt();
+
+        if (number < 1)
+            System.out.println("Neispravan unos!");
+        else {
+            System.out.println("Brojevi:");
+            for (int i = 1; i <= number; i++) {
+                if (i % sumaCifara(i) == 0)
+                    System.out.print(i + " ");
+
+                if (i == number) break;					//Escape za mogucu beskonacnu petlju
+            }
+        }
     }
 }
